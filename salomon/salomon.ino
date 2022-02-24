90536fdbee8b97512b52cbd9ed7670987f1c9ca8
Tochi <tochi.moreno@gmail.com>
2022-02-24 20:50:04 -0300

    NTP and RTC implemented

--- salomon/salomon.ino
+++ salomon/salomon.ino
@@ -1,4 +1,3 @@
-
 // Sistema de monitoreo de Luxis.
 
 // WiFi
@@ -11,46 +10,63 @@
 // RTC
 #include <RTClib.h>
 
-const char *ssid     = "<SSID>";
-const char *password = "<PASSWORD>";
+const char *ssid     = "ZOOROPA";
+const char *password = "m1lanesa";
+#define NTP_SERVER "time1.google.com"
+#define GMT_TIME_ZONE -3
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "time1.google.com", 3600, 60000);
+NTPClient timeClient(ntpUDP, NTP_SERVER, GMT_TIME_ZONE * 3600 , 60000);
 
 RTC_DS1307 rtc;
 char daysOfTheWeek[7][4] = {"DOM", "LUN", "MAR", "MIE", "JUE", "VIE", "SAB"};
 
 
+//---- Sincronizo el RTC con el NTP ----//
+void syncTime() {
+  
+  Serial.println( "Sync" );
+  timeClient.begin();
+  timeClient.update();
+
+  long actualTime = timeClient.getEpochTime();
+  Serial.println(timeClient.getFormattedDate());
+  Serial.println(timeClient.getFormattedTime());
+//  rtc.adjust(DateTime(actualTime));
+
+}
+
+
+
 void setup() {
+
+// Inicio Serial
   Serial.begin(115200);
 #ifndef ESP8266
   while (!Serial); // wait for serial port to connect. Needed for native USB
 #endif
 
+// Inicio WiFi
   WiFi.begin(ssid, password);
-
   while ( WiFi.status() != WL_CONNECTED ) {
     delay ( 500 );
-    Serial.print ( "." );
+//    Serial.print ( "." );
   }
+  Serial.println( "Conectado a WiFi " );
 
 // Inicio NTP
+  Serial.println( "Iniciando NTP" );
   timeClient.begin();
 
-// Inicio RTC
-  if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    Serial.flush();
-    while (1) delay(10);
-  }
+//// Inicio RTC
+  Serial.println( "Iniciando RTC" );
+//  if (! rtc.begin()) {
+//    Serial.println("Couldn't find RTC");
+//    Serial.flush();
+//    while (1) delay(10);
+//  }
 
-
-// Busca Hora
-  timeClient.update();
-  Serial.println(timeClient.getFormattedTime());
-
-  timeClient.
-  rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  syncTime();
 }
 
 void loop() {
